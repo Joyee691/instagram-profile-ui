b39a8e7e9567d6338f487569e9222fa8c5b10bd4
Joyee691 <58354394+Joyee691@users.noreply.github.com>
2020-10-08 17:38:57 +0800

    create whole framwork

--- src/ProfileTab.js
+++ src/ProfileTab.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { FiUserPlus } from "react-icons/fi";
-import { FaRegPaperPlane } from "react-icons/fa";
+import "./styles.css"
+import { AiOutlineLeft } from "react-icons/ai";
+import { BsThreeDots } from "react-icons/bs";
 
 export default function ProfileTab () {
-		return(
-			<div className="container">
-				<div className="header">
-					<div className="header-left"><FiUserPlus /></div>
-					<div className="header-body">Instagram</div>
-					<div className="header-right">
-						<FaRegPaperPlane />
-					</div>
-				</div>
+	{/* show more posts below */}
+	showMore=()=>{};
+
+	return(
+		<div className="container">
+			<div className="header">
+				<div className="header-left"><AiOutlineLeft /></div>
+				<div className="header-body">memopresso</div>
+				<div className="header-right"><BsThreeDots /></div>
 			</div>
-		);
+			<div className="content">
+				<div claseName="info"></div>
+				<div className="msg"></div>
+				<div className="contact"></div>
+				<div className="stories"></div>
+				<div className="buttons"></div>
+				<div className="posts"></div>
+				<button className="button" onClick={()=>showMore()}>Show more posts</button>
+			</div>
+		</div>
+	);
 }
